863eb4e6b9003165561365d37e76e12ec3596324
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-02-20 15:06:39 -0300

    convert tem function

--- cpp_tests/tests.cpp
+++ cpp_tests/tests.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
-#include <vector>
-using namespace std;
 
-int birthday(vector<int> s, int d, int m) {
-    int array_i = 0;
-    int result = 0;
-
-    while (array_i <= s.size() - m) { // Corrected loop condition
-        int sum = 0;
-
-        for (size_t i = array_i; i < m + array_i; ++i) {
-            sum += s[i];
-        }
-
-        if (sum == d) {
-            ++result;
-        }
-
-        ++array_i;
-    }
-
-    return result;
+double convertTemp(double celsius)
+{
+	return 1.8 * celsius + 32;
 }
 
-int main() {
+int main()
+{
 
-    vector<int> s{ 2, 2, 1, 3, 2 };
-    int d{ 4 };
-    int m{ 2 };
-    
-    std::cout << birthday(s, d, m);
+	double celsius{ 0 };
+	std::cout << "Please enter a degree value in celsius :\n";
+	std::cin >> celsius;
+	std::cout << celsius << "Celsius is " << convertTemp(celsius) << "Fahrenheit";
 
 	return 0;
 }
