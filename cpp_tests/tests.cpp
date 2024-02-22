45969f60a5bc9775b516c12bdd2c5eebc5883a78
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-02-22 14:48:51 -0300

    test

--- cpp_tests/tests.cpp
+++ cpp_tests/tests.cpp
@@ -1,23 +1,48 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
 
-	double lenght{ 0 };
-	double width{ 0 };
-	double height{ 0 };
+	std::vector<int> types_arr{ 0,0,0,0,0 };
+	std::vector<int> arr{ 1, 4, 4, 4, 5, 3 };
+
+	for (size_t i = 0; i < arr.size(); ++i)
+	{
+		switch (arr[i])
+		{
+		case 1:
+			types_arr[0] += 1;
+			break;
+		case 2:
+			types_arr[1] += 1;
+			break;
+		case 3:
+			types_arr[2] += 1;
+			break;
+		case 4:
+			types_arr[3] += 1;
+			break;
+		case 5:
+			types_arr[4] += 1;
+			break;
+		}
+	}
+
+	int higher_i{ 0 };
+	int higher_num = types_arr[higher_i];
 
 
-	std::cout << "Welcome to box calculator. Please type in lenght, width and height information :\n";
-	std::cout << "length: ";
-	std::cin >> lenght;
-	std::cout << "\nwidth: ";
-	std::cin >> width;
-	std::cout << "\nheight: ";
-	std::cin >> height;
-	std::cout << "\nThe base area is : " << lenght * width;
-	std::cout << "\nThe volume is: " << lenght * width * height;
+	for (size_t i = 1; i < types_arr.size(); ++i)
+	{
+		if (types_arr[i] > higher_num)
+		{
+			higher_num = types_arr[i];
+			higher_i = i;
+		}
+	}
 
+	std::cout << higher_i + 1;
 
 	return 0;
 }
