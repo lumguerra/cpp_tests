7cc1603fa145140a8ffa07176afb68a9cc161acd
Lucas Guerra <lucasmazoniguerra@gmail.com>
2024-03-01 18:09:10 -0300

    multiple inheritance

--- cpp_tests/tests.cpp
+++ cpp_tests/tests.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-void bonAppetit(vector<int> bill, int k, int b) 
-{
-    int ana_share = bill[k];
-    int bill_sum{ 0 };
-
-    for (size_t i = 0; i < bill.size(); ++i)
-    {
-        bill_sum += bill[i];
-    }
-
-    if (b == bill_sum)
-    {
-        cout << "Bon Appetit";
-    }
-    else
-    {
-        cout << b - ana_share;
-    }
-}
-
 int main()
 {
+	std::vector<int> vec{ 2,2,3,2,3,6,2 };
+	std::vector<int> pair_count;	
 
-	
+	for(size_t i = 0; i < vec.size(); ++i)
+	{
+		int sum{ 1 };
+
+		for (size_t j = ++i; j < vec.size(); ++j)
+		{						
+			if (vec[i] == vec[j])
+			{
+				++sum;
+			}			
+		}
+
+		if (sum > 1)
+		{
+			pair_count.push_back(sum / 2);
+		}
+	}
+
+	for (size_t i : pair_count)
+	{
+		std::cout << i << "\n";
+	}
 
 	return 0;
 }
